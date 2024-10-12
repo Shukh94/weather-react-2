1fe8347354769adf486cf19da0e98d2b48722636
Shukh94 <108126378+Shukh94@users.noreply.github.com>
2024-10-12 20:48:13 +0600

    added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WeatherSearch from "./WeatherSearch";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
+
+export default App;
